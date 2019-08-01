4d9fea0bf2b2f0aee45bf7bad1b8c0c537ae1ce4
Ashley Martin <amartin3112@gmail.com>
2019-08-01 17:42:26 +1000

    added content for events page

--- main.rb
+++ main.rb
@@ -1,11 +1,16 @@
-     
+require 'pry'
 require 'sinatra'
+require 'sinatra/reloader'
+require_relative 'database_config'
+
+require_relative 'models/user'
+require_relative 'models/event'
 
 get '/' do
   erb :index
 end
 
-
-
-
-
+get '/events' do
+  @events = Event.all
+  erb :events
+end
