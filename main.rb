dddedbaa08e073ce5d8448212f920e347a94a6a7
Ashley Martin <amartin3112@gmail.com>
2019-08-02 10:18:20 +1000

    created functionality to add new photos

--- main.rb
+++ main.rb
@@ -20,3 +20,21 @@ get '/photos' do
   @photos = Photo.all.order(:timestamp).reverse
   erb :photos
 end
+
+get '/photos/:id' do
+  erb :show_photo
+end
+
+get '/photos/add' do
+  erb :add_photo
+end
+
+post '/photos' do
+  photo = Photo.new
+  photo.image_url = params[:image_url]
+  photo.timestamp = Time.now # use timestamp from photo
+  photo.user_id = 1 # need to make this dynamic
+  # photo.event_id # optional
+  photo.save
+  redirect '/photos/add'
+end
