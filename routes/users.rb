8879f28fac45464e588b606d766991adb3d46dbf
Ashley Martin <amartin3112@gmail.com>
2019-08-03 16:48:42 +1000

    added functionality to edit user details

--- routes/users.rb
+++ routes/users.rb
@@ -2,6 +2,14 @@ get '/users' do
     erb :add_user
 end
 
+get '/users/:id/edit' do
+    erb :edit_user
+end
+
+get '/users/:id/edit_password' do
+    erb :edit_password
+end
+
 post '/users' do
     user = User.new
     user.first_name = params[:first_name]
@@ -10,4 +18,24 @@ post '/users' do
     user.password = params[:password]
     user.save
     redirect '/'
+end
+
+put '/users/:id/password' do
+    user = User.find(params[:id])
+    if user.authenticate(params[:current_password])
+        user.password = params[:password]
+        user.save
+        redirect "/users/#{user.id}/edit"
+    else
+        redirect "/users/#{user.id}/edit_password"
+    end
+end
+
+put '/users/:id' do
+    user = User.find(params[:id])
+    user.first_name = params[:first_name]
+    user.last_name = params[:last_name]
+    user.email = params[:email]
+    user.save
+    redirect '/events'
 end
