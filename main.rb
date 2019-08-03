92cdba9878d6b3538a29160a28e14e2b606099e6
Ashley Martin <amartin3112@gmail.com>
2019-08-03 12:09:40 +1000

    added helpers to simplify log in code

--- main.rb
+++ main.rb
@@ -10,6 +10,12 @@ require_relative 'models/photo'
 enable :sessions
 
 helpers do
+  def current_user
+    current_user = User.find_by(id: session[:user_id])
+  end
+  def logged_in?
+    !!current_user
+  end
 end
 
 after do
