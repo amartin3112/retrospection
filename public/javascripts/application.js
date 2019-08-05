459a51bd4ab78859c18ae6602802e3123a29f34c
Ashley Martin <amartin3112@gmail.com>
2019-08-05 14:22:56 +1000

    added confirmation step before deleting a photo

--- public/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-var deleteEvent = document.getElementById('delete-event');
-var deleteForm = document.getElementById('delete-form');
-
-var unhide = function() {
-    deleteForm.classList.remove('hide');
-};
-
-deleteEvent.addEventListener('click', unhide);
