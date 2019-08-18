c5309f74f3b446df34a6841a197223f1543df327
Ashley Martin <amartin3112@gmail.com>
2019-08-18 15:28:45 +1000

    added logic to delete associated events and photos when a user is deleted

--- models/user.rb
+++ models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     validates :first_name, :last_name, :email, presence: true
     validates :email, uniqueness: true
     validates :password, length: { in: 8..20 }
-    has_many :events
-    has_many :photos
+    has_many :events, dependent: :destroy
+    has_many :photos, dependent: :destroy
     has_secure_password
 end
